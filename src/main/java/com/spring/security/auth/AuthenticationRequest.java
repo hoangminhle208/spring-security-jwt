a4c9e99f7733b0963bfaa7280588de0ac87aa3ea
hoangminhle208 <hoanganhkks01@gmail.com>
2023-06-18 20:13:38 +0700

    push

---
+++ src/main/java/com/spring/security/auth/AuthenticationRequest.java
@@ -0,0 +1,15 @@
+package com.spring.security.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationRequest {
+    private String email;
+    String password;
+}
